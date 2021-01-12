327449b5d11bc001df40c6727e15c756c4f2e9fe
MatNoble <z-matrix@qq.com>
2021-01-12 19:12:01 +0800

    add 003 HashMap

---
+++ 003.py
@@ -0,0 +1,29 @@
+#==================================================
+#==>      Title: longest-substring-without-repeating-characters
+#==>     Author: Zhang zhen                   
+#==>      Email: hustmatnoble.gmail.com
+#==>     GitHub: https://github.com/MatNoble
+#==>       Date: 1/12/2021
+#==================================================
+
+"""
+https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+"""
+
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        i, j, cnt = 0, 0, 0
+        HashMap = {}
+        for idx, val in enumerate(s):
+            if HashMap.get(val) is not None:
+                i = max(HashMap.get(val), i)
+            HashMap[val] = idx + 1
+            cnt = max(cnt, idx-i+1)
+        return cnt
+
+s = 'abcabcbb'
+s = 'pwwkew'
+s = 'ewwkea'
+
+mat = Solution()
+mat.lengthOfLongestSubstring(s)
