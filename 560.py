acff3cadec115704f18b6d4978b3e9225209938e
MatNoble <z-matrix@qq.com>
2021-01-12 17:59:34 +0800

    add 560 HashMap

---
+++ 560.py
@@ -0,0 +1,26 @@
+#==================================================
+#==>      Title:                                    
+#==>     Author: Zhang zhen                   
+#==>      Email: hustmatnoble.gmail.com
+#==>     GitHub: https://github.com/MatNoble
+#==>       Date: 
+#==================================================
+
+
+class Solution:
+    def subarraySum(self, nums, k):
+        HashMap = {0:1}
+        sum_, cnt = 0, 0
+        for x in nums:
+            sum_ += x
+            temp = HashMap.get(sum_ - k)
+            if temp is not None:
+                cnt += temp
+            HashMap[sum_] = HashMap.get(sum_) + 1 if HashMap.get(sum_) is not None else 1 
+        return cnt
+
+nums = [3, 4, 7, 2, -3, 1, 4, 2]
+nums = [3, 4, 7, -7]
+k = 7
+mat = Solution()
+mat.subarraySum(nums, k)
