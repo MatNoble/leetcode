0623561cc9c0445f8ce525cbf27d0310f658ce71
MatNoble <z-matrix@qq.com>
2021-01-25 21:36:44 +0800

    add 066

---
+++ 066.py
@@ -0,0 +1,32 @@
+#==================================================
+#==>      Title: plus-one                                    
+#==>     Author: Zhang zhen                   
+#==>      Email: hustmatnoble.gmail.com
+#==>     GitHub: https://github.com/MatNoble
+#==>       Date: 1/25/2021
+#==================================================
+
+"""
+https://leetcode-cn.com/problems/plus-one/
+"""
+
+from typing import List
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)-1
+        while n >= 0:
+            digits[n] += 1
+            if digits[n] == 10:
+                digits[n] = 0
+                n -= 1
+            else:
+                break
+        if digits[0] == 0 and digits[-1] == 0:
+            digits.insert(0, 1)
+        return digits
+
+mat = Solution()
+digits = [9, 9, 9]
+digits = [0]
+digits = [0, 0]
+mat.plusOne(digits)
