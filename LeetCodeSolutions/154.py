1b029afea25ec20b54049e096121576b457422cd
MatNoble <z-matrix@qq.com>
2021-01-31 21:10:32 +0800

    add lc154 array

--- LeetCodeSolutions/154.py
+++ LeetCodeSolutions/154.py
@@ -1,21 +1,23 @@
 #==================================================
-#==>      Title: find-minimum-in-rotated-sorted-array
+#==>      Title: 154. 寻找旋转排序数组中的最小值 II
 #==>     Author: Zhang zhen                   
 #==>      Email: hustmatnoble.gmail.com
 #==>     GitHub: https://github.com/MatNoble
-#==>       Date: 1/22/2021
+#==>       Date: 1/31/2021
 #==================================================
 
 """
-https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/
 """
 
+from typing import List
 class Solution:
-    def findMin(self, nums):
+    def findMin(self, nums: List[int]) -> int:
         left, right = 0, len(nums)-1
         while left < right:
+            mid = left + (right - left) // 2
+            while left < mid and nums[left] == nums[mid]: left += 1
             if nums[left] < nums[right]: return nums[left]
-            mid = left + (right-left)//2
             if nums[left] <= nums[mid]:
                 left = mid+1
             else:
@@ -23,8 +25,8 @@ class Solution:
         return nums[left]
 
 mat = Solution()
-nums = [3,4,5,1,2]
-nums = [4,5,6,7,0,1,2]
-# nums = [1]
-nums = [2,3,0,1]
+nums = [2, 0, 2, 2]
+nums = [1, 2, 3]
+nums = [1]
+nums = [2, 0, 2, 2, 2, 2, 2]
 mat.findMin(nums)
