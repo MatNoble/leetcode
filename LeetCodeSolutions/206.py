6ed0460d43c51bcac07dbd968eb33e54dec6ef55
MatNoble <z-matrix@qq.com>
2021-02-02 19:26:15 +0800

    update 206 linkedlist

--- LeetCodeSolutions/206.py
+++ LeetCodeSolutions/206.py
@@ -17,9 +17,18 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
+    def reverseListR(self, head: ListNode) -> ListNode:
         if not head or head.next == None: return head
         res = self.reverseList(head.next)
         head.next.next = head
         head.next = None
         return res
+    
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur, pre = head, None
+        while cur:
+            tmp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = tmp
+        return pre
