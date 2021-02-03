02576af9dbb59ff0237589e47df96a3adb46da88
MatNoble <z-matrix@qq.com>
2021-02-03 16:36:56 +0800

    add 3 sliding window

--- LeetCodeSolutions/003.py
+++ LeetCodeSolutions/003.py
@@ -12,17 +12,32 @@ https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        i, cnt = 0, 0
-        HashMap = {}
-        for idx, val in enumerate(s):
-            if HashMap.get(val): i = max(HashMap.get(val), i)
-            HashMap[val] = idx + 1
-            cnt = max(cnt, idx-i+1)
-        return cnt
+        # i, cnt = 0, 0
+        # HashMap = {}
+        # for idx, val in enumerate(s):
+        #     if HashMap.get(val): i = max(HashMap.get(val), i)
+        #     HashMap[val] = idx + 1
+        #     cnt = max(cnt, idx-i+1)
+        # return cnt
 
-s = 'abcabcbb'
-s = 'pwwkew'
-s = 'ewwkea'
+        res = left = right = 0
+        hashMap = {}
+        while right < len(s):
+            if s[right] in hashMap:
+                res = max(res, right-left)
+                left = max(left, hashMap[s[right]])
+            hashMap[s[right]] = right+1
+            right += 1
+        res = max(res, right-left)
+        return res
+
+s = "abcabcbb"
+# s = "bbbbb"
+# s = "pwwkew"
+# s = ""
+# s = " "
+# s = "ab"
+# s = "abba"
 
 mat = Solution()
 mat.lengthOfLongestSubstring(s)
