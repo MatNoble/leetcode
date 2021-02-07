2047347b21970c1e487c4c88686ff8cac2023804
MatNoble <z-matrix@qq.com>
2021-02-07 13:02:42 +0800

    add 021

---
+++ LeetCodeSolutions/021.py
@@ -0,0 +1,36 @@
+#==================================================
+#==>      Title: 21. 合并两个有序链表
+#==>     Author: Zhang zhen                   
+#==>      Email: hustmatnoble.gmail.com
+#==>     GitHub: https://github.com/MatNoble
+#==>       Date: 2/7/2021
+#==================================================
+
+"""
+https://leetcode-cn.com/problems/merge-two-sorted-lists/
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        res = ListNode()
+        cur = res
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return res.next
+
+"""
+时间复杂度: O(max(m, n))
+空间复杂度: O(1)
+"""
