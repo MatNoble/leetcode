e7af6b19550f0427477857516e3ff9abce258966
MatNoble <z-matrix@qq.com>
2021-02-21 09:17:58 +0800

    update

--- Data Structures and Algorithms/recursion.py
+++ Data Structures and Algorithms/recursion.py
@@ -23,18 +23,50 @@ def factorial(n):
     return factorial(n-1) * n
 
 n = 3
-print(f'{n}! = {factorial(n)}')
+print(f'{n}! = {factorial(n)}\n')
 
 # ####################
 #  Fibonacci sequence
 # ####################
-def fib(n):
+def fibDP(n):  ## downUp
+    if n < 1: return 0
     if n <= 2: return 1
-    return fib(n-1) + fib(n-2)
+    dp = [0]*(n+1)
+    dp[1] = dp[2] = 1
+    i = 3
+    while i <= n:
+        dp[i] = dp[i-1] + dp[i-2]
+        i += 1
+    return dp[n]
 
-n = 5
+n = 10
+print('1, 1, 2, 3, 5, 8, 13 ....')
+print(f'第 {n} 位斐波那契数是 {fibDP(n)}')
+
+def fib(n):
+    if n < 1: return 0
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a+b
+    return a
+n = 10
 print('1, 1, 2, 3, 5, 8, 13 ....')
 print(f'第 {n} 位斐波那契数是 {fib(n)}')
+
+def fibRecursion(n):  ## upDown
+    def upDown(n):
+        if n < 1: return 0
+        if n <= 2:
+            return 1
+        if dp[n]: return dp[n]
+        dp[n] = upDown(n-1) + upDown(n-2)
+        return dp[n]
+    dp = [0] * (n+1)
+    return upDown(n)
+n = 10
+print('1, 1, 2, 3, 5, 8, 13 ....')
+print(f'第 {n} 位斐波那契数是 {fibRecursion(n)}\n')
+
 
 # ####################
 #     reverse String
--- Data Structures and Algorithms/tree.py
+++ Data Structures and Algorithms/tree.py
@@ -7,7 +7,7 @@
 #==================================================
 
 from typing import List
-
+import collections
 # Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -20,16 +20,18 @@ class Solution:
         if root is None: return []
         stack, res = [root], []
         while stack:
-            tmp = stack.pop()
-            res.append(tmp.val)
-            if tmp.right: stack.append(tmp.right)
-            if  tmp.left: stack.append(tmp.left)
+            node = stack.pop()
+            res.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
         return res
 
     def preorderTraversalRecursion(self, root: TreeNode) -> List[int]:
         def preorder(root):
-            if not root: return []
-            res.append(root.val)
+            if not root: return
+            res.append(root.val) ## 前序
             preorder(root.left)
             preorder(root.right)
             return res
@@ -44,16 +46,16 @@ class Solution:
             while cur:
                 stack.append(cur)
                 cur = cur.left
-            tmp = stack.pop()
-            res.append(tmp.val)
-            cur = tmp.right
+            node = stack.pop()
+            res.append(node.val)
+            cur = node.right
         return res
 
     def inorderTraversalRecursion(self, root: TreeNode) -> List[int]:
         def inorder(root):
-            if not root: return []
+            if not root: return
             inorder(root.left)
-            res.append(root.val)
+            res.append(root.val) ## 中序
             inorder(root.right)
             return res
         res = []
@@ -69,41 +71,100 @@ class Solution:
                 res.append(node.val)
             else:
                 stack.append((1, node))
-                if node.right: stack.append((0, node.right))
-                if  node.left: stack.append((0, node.left))
+                if node.right:
+                    stack.append((0, node.right))
+                if node.left:
+                    stack.append((0, node.left))
         return res
 
     def postorderTraversalRecursion(self, root: TreeNode) -> List[int]:
         def postorder(root):
-            if not root: return []
+            if not root: return
             postorder(root.left)
             postorder(root.right)
-            res.append(root.val)
+            res.append(root.val)  ## 后序
             return res
         res = []
         postorder(root)
         return res
 
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+    def levelTraversalLeft(self, root: TreeNode) -> List[List[int]]:
         if not root: return []
-        res, q = [], [root]
-        while q:
-            n = len(q)
-            level = []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            n, level = len(queue), []
             for _ in range(n):
-                node = q.pop(0)
+                node = queue.popleft()
                 level.append(node.val)
-                if  node.left: q.append(node.left)
-                if node.right: q.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
             res.append(level)
+        return res
+
+    def levelTraversalRight(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            n, level = len(queue), []
+            for _ in range(n):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+            res.append(level)
+        return res
+
+    def levelTraversal(self, root: TreeNode, k=-1) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            n, level = len(queue), collections.deque()
+            for _ in range(n):
+                node = queue.popleft()
+                if k == -1: ## 左 --> 右
+                    level.append(node.val)
+                else:       ## 右 --> 左
+                    level.appendleft(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(list(level))
+        return res
+
+    def levelTraversalJagged(self, root: TreeNode, k=-1) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            n, level = len(queue), collections.deque()
+            k *= -1
+            for _ in range(n):
+                node = queue.popleft()
+                if k == -1: ## 左 --> 右
+                    level.append(node.val)
+                else:       ## 右 --> 左
+                    level.appendleft(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(list(level))
         return res
 
     def maxDepth(self, root: TreeNode) -> int:
         def bottomUp(root):
             return 0 if not root else max(bottomUp(root.left), bottomUp(root.right)) + 1
-        def topDown(root, depth):
+        def topDown(root, depth=0):
             return depth if not root else max(topDown(root.left, depth+1), topDown(root.right, depth+1))
-        return topDown(root, 0), bottomUp(root)
+        return topDown(root), bottomUp(root)
 
 mat = Solution()
 ## 前序
@@ -115,6 +176,7 @@ root.left.right = TreeNode(4)
 
 root.right = TreeNode(5)
 root.right.left = TreeNode(6)
+
 print("\n前序输出：")
 print(mat.preorderTraversal(root))
 print(mat.preorderTraversalRecursion(root))
@@ -133,19 +195,19 @@ print(mat.inorderTraversal(root))
 print(mat.inorderTraversalRecursion(root))
 
 ## 后序
-root = TreeNode(6)
+# root = TreeNode(6)
 
-root.left = TreeNode(3)
-root.left.left = TreeNode(1)
-root.left.right = TreeNode(2)
+# root.left = TreeNode(3)
+# root.left.left = TreeNode(1)
+# root.left.right = TreeNode(2)
 
-root.right = TreeNode(5)
-root.right.left = TreeNode(4)
-print("\n后序输出：")
-print(mat.postorderTraversal(root))
-print(mat.postorderTraversalRecursion(root))
+# root.right = TreeNode(5)
+# root.right.left = TreeNode(4)
+# print("\n后序输出：")
+# print(mat.postorderTraversal(root))
+# print(mat.postorderTraversalRecursion(root))
 
-## 层序
+# ## 层序
 root = TreeNode(1)
 
 root.left = TreeNode(2)
@@ -155,7 +217,14 @@ root.left.right = TreeNode(5)
 root.right = TreeNode(3)
 root.right.left = TreeNode(6)
 print("\n层序输出：")
-print(mat.levelOrder(root))
+print(mat.levelTraversalLeft(root))
+print(mat.levelTraversalRight(root))
+
+print(mat.levelTraversal(root))
+print(mat.levelTraversal(root, 1))
+
+print(mat.levelTraversalJagged(root))
+print(mat.levelTraversalJagged(root, 1))
 
 print('\n最大树深度:')
 mat.maxDepth(root)
